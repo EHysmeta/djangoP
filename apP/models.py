34f5a7966b63ae30e6939ecc41007f83b0cc99db
EHysmeta <enes.hysmeta@icloud.com>
2023-09-14 11:12:40 +0200

    add about.html

--- apP/models.py
+++ apP/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    category_name = models.CharField(max_length=60,null=True, blank=True)
+    category_image = models.ImageField(upload_to='category/',null=True, blank=True)
+
 class Item(models.Model):
     item_id = models.AutoField(primary_key=True)
     item_title = models.CharField(max_length=250, null=True, blank=True)
@@ -8,3 +13,15 @@ class Item(models.Model):
     item_image = models.ImageField(upload_to='item/', null=True, blank=True)
     def __str__(self):
         return f'(self.iteml_title)'
+    
+class Staff(models.Model):
+    staff_id = models.AutoField(primary_key=True)
+    staff_name = models.CharField(max_length=60,null=True, blank=True)
+    staff_mbiemri = models.CharField(max_length=60, null=True, blank=True)
+    staff_image = models.ImageField(upload_to='staff/',null=True, blank=True)
+    staff_description = models.TextField(max_length=9000, null=True, blank=True)
+    staff_email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.staff_name} {self.staff_mbiemri}'
+    
--- apP/views.py
+++ apP/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home (request):
+    items = Item.objects.all()    
+    categorys = category.objects.all()
+    contex = {"items_value":items,"categorys_value":categorys}
+    return render(request,'home.html',contex)
+
+
+def about (request):
     items = Item.objects.all()
     contex = {"items_value":items}
-    return render(request,'home.html',contex)
+    return render(request,'about.html',contex)
+
+
 
 
     
